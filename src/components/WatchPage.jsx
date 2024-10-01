cf9b7d293897cadca8b9c3812cc36751faff2e96
Ayushman Singh <ayushmans012@gmail.com>
2024-10-01 23:45:22 +0530

    WatchPage Updated

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,29 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import { CHANNEL_API, VIDEO_API } from "../utils/constants";
+const KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"));
+  const [videoInfo , setVideoInfo] = useState(null);
+  const [channelImage , setChannelImage] = useState(null)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); //for closing the sidebar
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
+ 
+ 
+    useEffect( ()=>{            //for getting the information of the single video
+        getVideoInfo();
+      },[])
+
+  const getVideoInfo = async () => {
+    const data = await fetch(VIDEO_API+searchParams.get('v')+'&key='+KEY);
+    const json = await data.json();
+    setVideoInfo(json.items[0]);
+   // console.log(json.items[0]);
+    
+    //console.log(videoInfo); 
+  }
+ 
+  if (!videoInfo) return <div>Loading...</div>
+   
+  const { snippet, contentDetails, statistics } = videoInfo;
+  const { channelTitle, title , channelId } = snippet;
+ 
+  const getChannelImage = async() => {
+    const data =await fetch(CHANNEL_API+channelId+"&fields=items%2Fsnippet%2Fthumbnails&key="+KEY);
+    const json =await data.json();
+    console.log(json.items[0]?.snippet?.thumbnails?.medium.url);
+    setChannelImage(json.items[0]?.snippet?.thumbnails?.medium.url) 
+  }
+  getChannelImage();
+  
+  
+
   return (
-    <div>
+    <div className="m-10 ml-20"> 
       <iframe
-        width="1000"
-        height="550"
+        width="980"
+        height="480"
         src={"https://www.youtube.com/embed/"+searchParams.get('v')}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
-        className="rounded-xl m-10 ml-20"
+        className="rounded-xl "
       ></iframe>
+      <h1 className="font-bold text-xl m-2 ml-0">{title}</h1>
+      <div className="flex">
+      <img alt="Channel img" src={channelImage} className="rounded-full size-10"></img>
+      <h2 className="font-semibold self-center text-base m-2">{channelTitle}</h2>
+      </div>
+      
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,8 @@
 const KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key="+KEY
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key="+KEY;
+
+export const VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=";
+
+export const CHANNEL_API = "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="
+
