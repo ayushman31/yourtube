3994f892789b4908cf89dea60f1b990e2e518656
Ayushman Singh <ayushmans012@gmail.com>
2024-12-09 01:27:38 +0530

    feature where automatic logout if no token

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ButtonList from './ButtonList'
 import VideoContainer from './VideoContainer';
+import { useNavigate } from 'react-router-dom';
 
 const MainContainer = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!localStorage.getItem("token")){
+      navigate("/login")
+    }
+  }, [])
   return (
     <div>
         {/* <ButtonList /> */}
